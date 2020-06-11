b82fcf0c9414f055a4bf588892e6cf38435f0bb6
shirsa <shir@coqualabs.com>
2020-06-11 22:40:03 +0300

    Updates

---
+++ precache-manifest.31102ef5bd60d211864ebcc911bd4cd7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be4885fe7765f3733bb29814befc13d9",
+    "url": "/st-board-app/index.html"
+  },
+  {
+    "revision": "a2a7295f958f3955411c",
+    "url": "/st-board-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0f4322684dc11351a460",
+    "url": "/st-board-app/static/js/2.87cefc46.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/st-board-app/static/js/2.87cefc46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2a7295f958f3955411c",
+    "url": "/st-board-app/static/js/main.9e67aeb7.chunk.js"
+  },
+  {
+    "revision": "751a1389f96cac0bd0eb",
+    "url": "/st-board-app/static/js/runtime-main.c39e5728.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/st-board-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
